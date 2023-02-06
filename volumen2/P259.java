e93477363cb09d46970011670d28f123a78ff4ad
rimissgames <109381392+rimissgames@users.noreply.github.com>
2023-02-06 20:09:49 +0100

    Nuevos retos resueltos
    Actualización de los retos

---
+++ volumen2/P259.java
@@ -0,0 +1,35 @@
+package volumen2;
+
+import java.util.Scanner;
+
+public class P259 {
+	public static void main(String[] args) {
+		long numNinos, numCaramelos, caramelos;
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			caramelos = sc.nextLong();
+			if (caramelos == 0) {
+				break;
+			}
+			numNinos = 0;
+			numCaramelos = 0;
+			numCaramelos = numCaramelos + caramelos;
+			numNinos++;
+			while (true) {
+				caramelos = sc.nextLong();
+				if (caramelos == 0) {
+					break;
+				}
+				numCaramelos = numCaramelos + caramelos;
+				numNinos++;
+			}
+			if (numCaramelos % numNinos != 0) {
+				System.out.printf("IMPOSIBLE\n");
+			} else {
+				System.out.printf("REPARTO JUSTO\n");
+			}
+		}
+		return;
+	}
+}
